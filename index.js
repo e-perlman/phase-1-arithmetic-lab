b741de21e85debfe4514d3ee832eb1c58de117ef
Emma Perlman <emmajperlman@gmail.com>
2022-02-07 18:15:02 -0800

    commit tree 8609ba137f887ba681315fdff46416e6e9c59b9c

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+const num1=62
+const num2=1
+const num3=14
+const num4=5
+
+const multiply=num1*num2
+
+const random=Math.round(Math.random())+1
+
+const mod=num3%num4
+
+const max=Math.max(3,4,20)
